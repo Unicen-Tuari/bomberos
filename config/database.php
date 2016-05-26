9abdf8c424400fa6301966ed4d534e25b75ef6fb
ferochoteco <fernandoochoteco@gmail.com>
2016-05-26 14:34:17 -0300

    dddsdddasddddffff

--- config/database.php
+++ config/database.php
@@ -1,5 +1,10 @@
 <?php
 
+    $url = parse_url(getenv("DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
 return [
 
     /*
@@ -30,18 +35,13 @@ return [
 
     // 'default' => 'pgsql',
 
-    $url = parse_url(getenv("DATABASE_URL"));
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
     //
     // 'pgsql' => array(
     // 'driver'   => 'pgsql',
-    'host'     => $host,
-    'database' => $database,
-    'username' => $username,
-    'password' => $password,
+    // 'host'     => $host,
+    // 'database' => $database,
+    // 'username' => $username,
+    // 'password' => $password,
     // 'charset'  => 'utf8',
     // 'prefix'   => '',
     // 'schema'   => 'public',
