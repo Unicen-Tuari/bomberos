ac35123ebce8ae9db5f8b00061df6bfa5cb4a122
ferochoteco <fernandoochoteco@gmail.com>
2016-05-26 14:31:08 -0300

    dddsddd

--- config/database.php
+++ config/database.php
@@ -26,26 +26,26 @@ return [
     |
     */
 
-    //'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
-     'default' => 'pgsql',
+    // 'default' => 'pgsql',
 
-    $url = parse_url(getenv("DATABASE_URL"));
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
-
-    'pgsql' => array(
-    'driver'   => 'pgsql',
-    'host'     => $host,
-    'database' => $database,
-    'username' => $username,
-    'password' => $password,
-    'charset'  => 'utf8',
-    'prefix'   => '',
-    'schema'   => 'public',
-    ),
+    // $url = parse_url(getenv("DATABASE_URL"));
+    // $host = $url["host"];
+    // $username = $url["user"];
+    // $password = $url["pass"];
+    // $database = substr($url["path"], 1);
+    //
+    // 'pgsql' => array(
+    // 'driver'   => 'pgsql',
+    // 'host'     => $host,
+    // 'database' => $database,
+    // 'username' => $username,
+    // 'password' => $password,
+    // 'charset'  => 'utf8',
+    // 'prefix'   => '',
+    // 'schema'   => 'public',
+    // ),
 
     /*
     |--------------------------------------------------------------------------
@@ -63,48 +63,48 @@ return [
     |
     */
 
-    // 'connections' => [
-    //
-    //     'sqlite' => [
-    //         'driver'   => 'sqlite',
-    //         'database' => database_path('database.sqlite'),
-    //         'prefix'   => '',
-    //     ],
-    //
-    //     'mysql' => [
-    //         'driver'    => 'mysql',
-    //         'host'      => env('DB_HOST', 'localhost'),
-    //         'database'  => env('DB_DATABASE', 'forge'),
-    //         'username'  => env('DB_USERNAME', 'forge'),
-    //         'password'  => env('DB_PASSWORD', ''),
-    //         'charset'   => 'utf8',
-    //         'collation' => 'utf8_unicode_ci',
-    //         'prefix'    => '',
-    //         'strict'    => false,
-    //     ],
-    //
-    //     'pgsql' => [
-    //         'driver'   => 'pgsql',
-    //         'host'     => env('DB_HOST', 'localhost'),
-    //         'database' => env('DB_DATABASE', 'forge'),
-    //         'username' => env('DB_USERNAME', 'forge'),
-    //         'password' => env('DB_PASSWORD', ''),
-    //         'charset'  => 'utf8',
-    //         'prefix'   => '',
-    //         'schema'   => 'public',
-    //     ],
-    //
-    //     'sqlsrv' => [
-    //         'driver'   => 'sqlsrv',
-    //         'host'     => env('DB_HOST', 'localhost'),
-    //         'database' => env('DB_DATABASE', 'forge'),
-    //         'username' => env('DB_USERNAME', 'forge'),
-    //         'password' => env('DB_PASSWORD', ''),
-    //         'charset'  => 'utf8',
-    //         'prefix'   => '',
-    //     ],
-    //
-    // ],
+    'connections' => [
+
+        'sqlite' => [
+            'driver'   => 'sqlite',
+            'database' => database_path('database.sqlite'),
+            'prefix'   => '',
+        ],
+
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
+        'pgsql' => [
+            'driver'   => 'pgsql',
+            'host'     => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ],
+
+        'sqlsrv' => [
+            'driver'   => 'sqlsrv',
+            'host'     => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset'  => 'utf8',
+            'prefix'   => '',
+        ],
+
+    ],
 
     /*
     |--------------------------------------------------------------------------
