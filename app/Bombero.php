485e83781d8fb107d9d8c9d5a9fb2b4a5c6647a8
ferochoteco <fernandoochoteco@gmail.com>
2016-08-30 09:33:12 -0300

    adsads

--- app/Bombero.php
+++ app/Bombero.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Bombero extends Model
 {
   protected $table = 'bombero';
-  protected $fillable = 'nombre';
+  protected $fillable = [
+    'id', 'nombre', 'apellido', 'nro_legajo', 'jerarquia', 'direccion', 'telefono', 'fecha_nacimiento',
+  ];
 }
