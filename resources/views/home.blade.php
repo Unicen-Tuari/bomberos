9db5e31e99c1c914e6eb6f8f505243e9435b885a
Pablo-Rivera <paliyo@gmail.com>
2016-12-06 10:34:20 -0300

    heroku

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,9 @@
       <div class="col-md-10 col-md-offset-1">
           <div class="panel panel-default">
               <div class="panel-heading">Dashboard</div>
-            
+
               <div class="panel-body">
-                  Adentro!
+                  Manual de ayuda!
               </div>
           </div>
       </div>
