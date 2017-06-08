aede57769ea4e86ff59af74ada8daeb255961445
Pablo-Rivera <paliyo@gmail.com>
2017-06-08 16:29:08 -0300

    arreglo en factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -30,7 +30,7 @@ $factory->define(App\Bombero::class, function (Faker\Generator $faker) {
     'jerarquia' => $faker->numberBetween($min = 1, $max = 6),
     'direccion' => $faker->address,
     'telefono' => $faker->e164PhoneNumber,
-    'fecha_nacimiento' => $faker->date($format = 'd/m/Y', $max = '1990-01-01'),
+    'fecha_nacimiento' => $faker->date($format = 'd/m/Y', $max = '01/01/1990'),
   ];
 
 });
