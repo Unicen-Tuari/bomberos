c333dee5f5ded4303bee3f3c945fa721ae695c07
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-10-08 22:44:46 -0300

    Aun le falta algunos arreglos ABMTestVehiculo

--- tests/Browser/ABMVehiculoTest.php
+++ tests/Browser/ABMVehiculoTest.php
@@ -2,30 +2,92 @@
 
 namespace Tests\Browser;
 
+use Illuminate\Database\Seeder;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Database;
 use App\Vehiculo;
+use App\User;
 
 class ABMVehiculoTest extends DuskTestCase
 {
+    private $vehiculo;
+    private $usuario;
+    private $password;
+    private $vehiculoEdit;
+
+    function setUp()
+    {
+      parent::setUp();
+      $this->vehiculo = Vehiculo::create(['patente' => 'AAA 999','num_movil' => '30','estado'=>'2','detalle'=>'autobomba']);
+      $this->vehiculoEdit = Vehiculo::create(['patente' => 'EEE 999','num_movil' => '31','estado'=>'2','detalle'=>'autobomba']);
+      //$this->vehiculo = factory(Vehiculo::class)->create(['patente' => 'WR 98 PO','num_movil' => '30','estado'=>'1','detalle'=>'autobomba']);
+      //$this->vehiculoEdit = factory(Vehiculo::class)->create(['patente' => 'WRO 988','num_movil' => '27','estado'=>'2','detalle'=>'autobomba']);
+      //Vehiculo::create(['patente' => 'WR 989 PO','num_movil' => '11','estado'=>'1','detalle'=>'autobomba']);
+      $this->usuario = User::find(1)->usuario;
+      $this->password = 'nico1234';
+    }
+
+    function tearDown()
+    {
+        $this->vehiculo->delete();
+        $this->vehiculoEdit->delete();
+    }
+
     /**
      * A Dusk test example.
      *
      * @return void
      */
-    public function createVehiculoTest()
+
+     /*public function testLoginUser()
+     {
+       $user = User::find(1);
+       $this->browse(function ($browser) use ($user) {
+       $browser->loginAs($user)
+               ->visit('/vehiculo/create')
+               ->assertSee('vehiculo');
+     });
+   }*/
+
+
+    public function testCreateVehicle()
     {
-      $vehiculo = factory(App\vehiculo::class)->create(['patente' => 'KJD 456', 'num_movil' => '12', 'estado' => '1', 'detalle' => 'autobomba']);
-            $this->browse(function (Browser $browser) {
-            $browser->visit('/vehiculo/create')
-                    ->type('Patente',$vehiculo->patente)
-                    ->type('num_movil',$vehiculo->num_movil)
-                    ->type('estado',$vehiculo->estado)
-                    ->type('detalle',$vehiculo->detalle)
-                    ->press('Registrar')
-                    ->assertSee('Vehiculo');
+        $this->browse(function (Browser $browser) {
+        $browser->visit('/vehiculo/create')
+                ->type('usuario',$this->usuario)
+                ->type('password',$this->password)
+                ->press('Iniciar')
+                ->type('patente',$this->vehiculo->patente)
+                ->type('num_movil',$this->vehiculo->num_movil)
+                ->select('estado',$this->vehiculo->estado)
+                ->type('detalle',$this->vehiculo->detalle)
+                ->press('Registrar')
+                ->assertSee('Buscar');
         });
     }
+
+    public function testModifyVehicle()
+    {
+      $this->browse(function (browser $browser){
+      $browser->visit('/vehiculo/9/edit')
+              ->type('usuario', $this->usuario)
+              ->type('password', $this->password)
+              ->press('Iniciar')
+              ->type('patente', $this->vehiculoEdit->patente)
+              ->type('num_movil', $this->vehiculoEdit->num_movil)
+              ->select('estado',$this->vehiculoEdit->estado)
+              ->type('detalle',$this->vehiculoEdit->detalle)
+              ->press('Editar')
+              ->assertSee('Buscar');
+      });
+    }
+
+   /*public function testDeleteVehicle()
+   {
+     $this->browse(function(browser $browser){
+     $browser->visit('/vehiculo/9/delete')
+              ->assertSee('Buscar');
+     });
+   }*/
 }
