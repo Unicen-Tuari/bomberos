af1e0e3e887a4d474c1c2201aa8a4dc5fbb48a18
SSAAMMAA <facundo.samartino@gmail.com>
2017-10-17 20:40:10 -0300

    errores de sqllite

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,14 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-<<<<<<< HEAD
-       if ($this->app->environment('local', 'testing')) {
-         $this->app->register(DuskServiceProvider::class);
-       }
-=======
+
       if ($this->app->environment('local', 'testing')) {
          $this->app->register(DuskServiceProvider::class);
       }
->>>>>>> cae0a375321afff2d8b71c4b6c2d5dc052c4b22a
     }
 }
