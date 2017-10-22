162541299af24b7de20cacba550157161315431f
Ignacio Jonas <ignaciojonas@gmail.com>
2017-10-22 15:48:18 -0300

    Add Travis-CI to build the solution

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -29,15 +29,15 @@ abstract class DuskTestCase extends BaseTestCase
      */
     protected function driver()
     {
-        $options = (new ChromeOptions)->addArguments([
-            '--disable-gpu',
-            '--headless'
-        ]);
-
-        return RemoteWebDriver::create(
-            'http://selenium:4444/wd/hub', DesiredCapabilities::chrome()->setCapability(
-                ChromeOptions::CAPABILITY, $options
-            )
-        );
-    }  
+      $options = (new ChromeOptions)->addArguments([
+          '--disable-gpu',
+          '--headless',
+          '--window-size=1100,600',
+      ]);
+      return RemoteWebDriver::create(
+          'http://localhost:9515', DesiredCapabilities::chrome()->setCapability(
+              ChromeOptions::CAPABILITY, $options
+          )
+      );
+    }
 }
