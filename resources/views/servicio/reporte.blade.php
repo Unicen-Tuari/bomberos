a8c52a7e2acd77560c08e97b61e3ccec837d13c9
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-10-29 15:48:22 -0300

    Se creo la vista de reportes para imprimir

---
+++ resources/views/servicio/reporte.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+
+<article class="col-sm-12">
+  <div class="panel panel-default">
+    <div id="breadcrumb" class="panel-heading">
+      <span class="fa fa-print" aria-hidden="true"></span>
+      <h4>Reportes</h4>
+    </div>
+    <div class="panel-body form-horizontal">
+      <div class="form-group col-sm-3">
+        {{ Form::label('me', 'Mes:',['class' => 'control-label col-sm-3']) }}
+        <div class="col-sm-9">
+          {{Form::select('mes', config('selects.meses'),\Carbon\Carbon::now()->format('m'), ['class' => 'form-control','id' => 'mes'])}}
+        </div>
+      </div>
+      <div class="form-group col-sm-3">
+        {{ Form::label('añ', 'Año:',['class' => 'control-label col-sm-3']) }}
+        <div class="col-sm-9">
+          {{Form::text('año', \Carbon\Carbon::now()->format('Y'), ['class' => 'form-control','id'=>'año'])}}
+        </div>
+      </div>
+			<div id="reportes">
+
+			</div>
+    </div>
+  </div>
+</article>
+@endsection
+
+@section('js')
+  {!! Html::script('assets/js/ajaxtablareportes.js') !!}
+@endsection
