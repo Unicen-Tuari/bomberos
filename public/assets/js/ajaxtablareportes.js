1a3b37c6678738ab87f959481e49ab9c820a4897
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-10-29 15:47:52 -0300

    Se hizo un nuevo js para generar la tabla de reportes a imprimir

---
+++ public/assets/js/ajaxtablareportes.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  function cargarTabla(){
+    var mes=$('#mes').val();
+    var año=$('#año').val();
+		$.ajax({
+			type: 'GET',
+			dataType: 'HTML',
+			url: '../servicio/'+mes+'/'+año+'/tabla',
+			success: function(data){
+						$('#reportes').html(data);
+					},
+			error: function(){
+						alert('Error al Cargar la tabla ');
+					}
+		});
+  };
+
+  $('#mes').on('change',function(){
+    cargarTabla();
+  });
+
+  $('#año').on('change',function(){
+    cargarTabla();
+  });
+  cargarTabla();
+});
