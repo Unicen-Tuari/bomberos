dfcfd9b68365a5b6683c600ed18dfc2568ca57ef
Jeremias Perez <jereprz1994@gmail.com>
2017-10-30 13:53:41 -0300

    Test ABM asistencia. Error en el delete y el update si no hay ninguna reunion cargada, ya que no hay nada que modificar o borrar y Error en Create si no hay bomberos para agregar a la reunion. closes #169

---
+++ tests/Browser/AssistanceTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
+use App\Asistencia;
+
+class Assistancetest extends DuskTestCase{
+  private $usuarioAdmin;
+  private $password;
+
+  function setUp()
+  {
+    parent::setUp();
+    $this->asistencia = factory(Asistencia::class)->make();
+    $this->usuarioAdmin=factory(User::class)->create(['admin'=> '1', 'password'=> bcrypt('123456')]);
+    $this->password = '123456';
+    $this->browse(function (Browser $browser) {
+      $browser->visit('/login')
+      ->type('usuario', $this->usuarioAdmin->usuario)
+      ->type('password', $this->password)
+      ->press('Iniciar');
+    });
+  }
+
+  public function testCreate()
+  {
+    $this->browse(function (Browser $browser) {
+      $browser->visit('/asistencia/create')
+      ->type('fecha_reunion', $this->asistencia->fecha_reunion)
+      ->click('.off')
+      ->press('Finalizar')
+      ->assertSee('Reunión del ' . $this->asistencia->fecha_reunion);
+    });
+  }
+
+  public function testUpdate()
+  {
+    $this->browse(function (Browser $browser) {
+      $browser->visit('/asistencia')
+      ->click('.glyphicon-edit')
+      ->click('.btn-success')
+      ->press('Finalizar')
+      ->assertDontSee('Reunión del ' . $this->asistencia->fecha_reunion);
+    });
+  }
+
+  public function testDelete()
+  {
+    $this->browse(function (Browser $browser) {
+      $browser->visit('/asistencia')
+      ->click('.glyphicon-trash')
+      ->assertDontSee('Reunión del ' . $this->asistencia->fecha_reunion);
+    });
+  }
+
+
+
+
+}
