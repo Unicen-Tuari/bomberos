c85ba3aacc14e4d98f0d7f21e96319921a445d43
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-08 00:53:17 -0300

    creando planilla de reportes

--- resources/views/servicio/reporte.blade.php
+++ resources/views/servicio/reporte.blade.php
@@ -6,42 +6,95 @@
     <table class="table table-bordered">
         <tr>
           <td rowspan=2>B.V.</td>
-          <td rowspan=2>TRENQUE LAUQUEN</td>
+          <td colspan=14 rowspan=2>TRENQUE LAUQUEN</td>
           <td colspan=2>INOBV</td>
-          <td>RENI</td>
+          <td colspan=2>RENI</td>
           <td></td>
         </tr>
           <tr>
-            <td>3</td>
-            <td>3</td>
-            <td colspan=2>BA</td>
+            <td colspan=1>3</td>
+            <td colspan=1>3</td>
+            <td colspan=3>BA</td>
           </tr>
-          <tr>
-            <td rowspan=2>PARTE DE SERVICIO</td>
-          </tr>
-    </table>
-
-    <table class="table table-bordered">
-      <tr>
         <tr>
-          <td rowspan=2>B.V.</td>
-          <td rowspan=2>TRENQUE LAUQUEN</td>
-          <td colspan=2>INOBV</td>
-          <td>RENI</td>
+          <td rowspan=2>PARTE DE SERVICIO</td>
+          <td >01</td>
+          <td >02</td>
+          <td >03</td>
+          <td >04</td>
+          <td >05</td>
+          <td >06</td>
+          <td >07</td>
+          <td >08</td>
+          <td >09</td>
+          <td >10</td>
+          <td >11</td>
+          <td >DIA</td>
+          <td >MES</td>
+          <td >AÑO</td>
+          <td colspan=2 rowspan=2>P.S.N°</td>
+          <td rowspan=2></td>
+          <td rowspan=2></td>
+          <td rowspan=2></td>
+        </tr>
+        <tr>
+          <td><br></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
           <td></td>
         </tr>
-          <tr>
-            <td>3</td>
-            <td>3</td>
-            <td colspan=2>BA</td>
-          </tr>
-      </tr>
-          <tr>
-            <td rowspan=2>PARTE DE SERVICIO</td>
-            <td rowspan=2>PARTE DE SERVICIO</td>
-            <td rowspan=2>PARTE DE SERVICIO</td>
-            <td rowspan=2>PARTE DE SERVICIO</td>
-          </tr>
+        <tr>
+          <td>TIPO</td>
+          <td colspan=14></td>
+          <td colspan=2 rowspan=2>PRESENTE 01 AL 05</td>
+          <td rowspan=2></td>
+          <td rowspan=2></td>
+          <td rowspan=2></td>
+        </tr>
+        <tr>
+          <td>UBICACION</td>
+          <td colspan=14></td>
+        </tr>
+        <tr>
+          <td>HORA DE ALARMA</td>
+          <td colspan=3>HORA DE SALIDA</td>
+          <td colspan=2>MOVIL</td>
+          <td colspan=7>A CARGO</td>
+          <td colspan=2>HORA DE REGRESO</td>
+          <td colspan=2 rowspan=2>DURACION EN MINUTOS</td>
+          <td rowspan=2></td>
+          <td rowspan=2></td>
+          <td rowspan=2></td>
+        </tr>
+        <tr>
+          <td><br></td>
+          <td colspan=3></td>
+          <td colspan=2></td>
+          <td colspan=7></td>
+          <td colspan=2></td>
+        </tr>
+        <tr>
+          <td colspan=4>N° DE MOVILES</td>
+          <td colspan=3>ILESOS</td>
+          <td colspan=4>MUERTOS</td>
+          <td colspan=2>LESIONADOS</td>
+          <td colspan=2 rowspan=2>MINUTOS HOMBRE</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
     </table>
       </div>
 @endsection
