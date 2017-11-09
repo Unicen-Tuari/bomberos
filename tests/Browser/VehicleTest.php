d6414f2baf0766627809f598b694b10d2e293f62
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-09 14:26:54 -0300

    Refactor VehicleTest

--- tests/Browser/VehicleTest.php
+++ tests/Browser/VehicleTest.php
@@ -25,46 +25,40 @@ class VehicleTest extends DuskTestCase
       $this->password = '123456';
   }
 
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
+  public function testCreate()
+  {
+    $this->browse(function (Browser $browser) {
+      $patente = strtoupper($this->vehiculo->patente);
+      $browser->loginAs($this->usuarioAdmin)
+              ->visit('/vehiculo/create')
+              ->type('patente',$this->vehiculo->patente)
+              ->type('num_movil',$this->vehiculo->num_movil)
+              ->select('estado',$this->vehiculo->estado)
+              ->type('detalle',$this->vehiculo->detalle)
+              ->press('Registrar')
+              ->type('patente',$patente)
+              ->press('Buscar')
+              ->assertSee($this->vehiculo->num_movil);
+    });
+  }
 
-    public function testCreate()
-    {
-        $this->browse(function (Browser $browser) {
-          $patente = strtoupper($this->vehiculo->patente);
-          $browser->loginAs($this->usuarioAdmin)
-                  ->visit('/vehiculo/create')
-                  ->type('patente',$this->vehiculo->patente)
-                  ->type('num_movil',$this->vehiculo->num_movil)
-                  ->select('estado',$this->vehiculo->estado)
-                  ->type('detalle',$this->vehiculo->detalle)
-                  ->press('Registrar')
-                  ->type('patente',$patente)
-                  ->press('Buscar')
-                  ->assertSee($this->vehiculo->num_movil);
-          });
-    }
-
-    public function testUpdate()
-    {
-      $this->browse(function (browser $browser){
-        $patente = strtoupper($this->vehiculoEdit->patente);
-        $browser->loginAs($this->usuarioAdmin)
-                ->visit('/vehiculo')
-                ->click('.glyphicon-edit')
-                ->type('patente', $this->vehiculoEdit->patente)
-                ->type('num_movil', $this->vehiculoEdit->num_movil)
-                ->select('estado',$this->vehiculoEdit->estado)
-                ->type('detalle',$this->vehiculoEdit->detalle)
-                ->press('Editar')
-                ->type('patente', $patente)
-                ->press('Buscar')
-                ->assertSee($this->vehiculoEdit->num_movil);
-        });
-    }
+  public function testUpdate()
+  {
+    $this->browse(function (browser $browser){
+      $patente = strtoupper($this->vehiculoEdit->patente);
+      $browser->loginAs($this->usuarioAdmin)
+              ->visit('/vehiculo')
+              ->click('.glyphicon-edit')
+              ->type('patente', $this->vehiculoEdit->patente)
+              ->type('num_movil', $this->vehiculoEdit->num_movil)
+              ->select('estado',$this->vehiculoEdit->estado)
+              ->type('detalle',$this->vehiculoEdit->detalle)
+              ->press('Editar')
+              ->type('patente', $patente)
+              ->press('Buscar')
+              ->assertSee($this->vehiculoEdit->num_movil);
+    });
+  }
 
   public function testDelete()
   {
