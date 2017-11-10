306f8611809bf0c23ee02f4f1b3ad9b251243089
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-10 18:59:29 -0300

    Vista para mostrar planilla de reporte de un dia (falta terminar)

--- resources/views/servicio/reporte.blade.php
+++ resources/views/servicio/reporte.blade.php
@@ -1,287 +1,105 @@
 @extends('layouts.app')
 @section('content')
-  <div id="breadcrumb" class="panel-heading">
-    <span class="fa fa-print" aria-hidden="true"></span>
-    <h4>Reporte de Servicio</h4>
-  </div>
-  <div class="container-fluid" id="planillaReporteServicio">
-    <div class="row">
-      <div class="col-xs-12 col-md-12 table-responsive">
-        <table class="text-center table table-bordered">
-          <tr>
-            <td rowspan=2>B.V.</td>
-            <td class="text-center" colspan=14 rowspan=2>TRENQUE LAUQUEN</td>
-            <td colspan=2>INOBV</td>
-            <td colspan=2>RENI</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td colspan=1>3</td>
-            <td colspan=1>3</td>
-            <td class="col-md-2" colspan=3>BA</td>
-          </tr>
-          <tr>
-            <td rowspan=2>PARTE DE SERVICIO</td>
-            <td>01</td>
-            <td>02</td>
-            <td>03</td>
-            <td>04</td>
-            <td>05</td>
-            <td>06</td>
-            <td>07</td>
-            <td>08</td>
-            <td>09</td>
-            <td>10</td>
-            <td>11</td>
-            <td>DIA</td>
-            <td>MES</td>
-            <td>AÑO</td>
-            <td colspan=2 rowspan=2>P.S.N°</td>
-            <td class="col-md-1" rowspan=2></td>
-            <td class="col-md-1" rowspan=2></td>
-            <td class="col-md-1" rowspan=2></td>
-          </tr>
-          <tr>
-            <td colspan=1><br></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>TIPO</td>
-            <td colspan=14></td>
-            <td colspan=2 rowspan=2>PRESENTE 01 AL 05</td>
-            <td rowspan=2></td>
-            <td rowspan=2></td>
-            <td rowspan=2></td>
-          </tr>
-          <tr>
-            <td>UBICACION</td>
-            <td colspan=14></td>
-          </tr>
-          <tr>
-            <td>HORA DE ALARMA</td>
-            <td colspan=3>HORA DE SALIDA</td>
-            <td colspan=2>MOVIL</td>
-            <td colspan=7>A CARGO</td>
-            <td colspan=2>HORA DE REGRESO</td>
-            <td colspan=2 rowspan=2><p>DURACION EN MINUTOS</p> </td>
-            <td rowspan=2></td>
-            <td rowspan=2></td>
-            <td rowspan=2></td>
-          </tr>
-          <tr>
-            <td><br></td>
-            <td colspan=3></td>
-            <td colspan=2></td>
-            <td colspan=7></td>
-            <td colspan=2></td>
-          </tr>
-          <tr>
-            <td colspan=4>N° DE MOVILES</td>
-            <td colspan=3>ILESOS</td>
-            <td colspan=4>MUERTOS</td>
-            <td colspan=2>LESIONADOS</td>
-            <td colspan=2 rowspan=2>MINUTOS HOMBRE</td>
-            <td colspan=1 rowspan=2></td>
-            <td colspan=1 rowspan=2></td>
-            <td rowspan=2></td>
-            <td rowspan=2></td>
-            <td rowspan=2></td>
-          </tr>
-          <tr>
-            <td colspan=4 rowspan=3></td>
-            <td colspan=3><br></td>
-            <td colspan=4></td>
-            <td colspan=2></td>
-          </tr>
-          <tr>
-            <td colspan=3>SUP (HAS)</td>
-            <td colspan=4>QUEMADOS</td>
-            <td colspan=2>OTROS</td>
-            <td colspan=2>COMBUSTIBLE</td>
-            <td colspan=5>COLABORO CUARTEL</td>
-          </tr>
-          <tr>
-            <td colspan=3><br></td>
-            <td colspan=4></td>
-            <td colspan=2></td>
-            <td colspan=2></td>
-            <td colspan=5></td>
-          </tr>
-          <tr>
-            <td class="text-left" colspan=1>ALARMA</td>
-            <td class="text-left" colspan=2>GRAL.</td>
-            <td class="text-left" colspan=2>SEL. </td>
-            <td class="text-left" colspan=2>INT.</td>
-            <td class="text-left" colspan=15>AUTOR LLAMADA</td>
-          </tr>
-          <tr>
-            <td class="col-md-1 text-left" colspan=20>RECONOCIMIENTO:</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1 text-left" colspan=20>DISPOSICIONES:</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td class="col-md-1" colspan=20>&#160;</td>
-          </tr>
-          <tr>
-            <td colspan=11> OPERADOR DE COMANDO</td>
-            <td colspan=9>JEFE DEL SERVICIO</td>
-          </tr>
-          <tr>
-            <td colspan=11><br><br><br></td>
-            <td colspan=9><br><br><br></td>
-          </tr>
-          <tr>
-            <td colspan=11>FIRMA Y N° DE LEGAJO</td>
-            <td colspan=9>FIRMA Y N° DE LEGAJO</td>
-          </tr>
-        </table>
-        <div class="col-xs-12 col-md-12">
-          <p>01: Incendio &#160;&#160;&#160; - &#160; 02: Auxilio &#160;&#160;&#160; - &#160; 03: Especiales &#160;&#160;&#160; - &#160; 04: Desastre &#160;&#160;&#160; - &#160; 05: Colaboración &#160;&#160;&#160; - &#160; 06: Guardias <br>
-            07: Comando &#160;&#160;&#160; - &#160; 08: Tecnico &#160;&#160;&#160; - &#160; 09: Ceremonial &#160;&#160;&#160; - &#160; 10: Productos Quimicos &#160;&#160;&#160; - &#160; 11: Forestales
-          </p>
-        </div>
+  <article class="col-sm-12">
+    <div class="panel panel-default">
+      <div id="breadcrumb" class="panel-heading">
+        <span class="fa fa-print" aria-hidden="true"></span>
+        <h4>Reporte de Servicio</h4>
       </div>
-    </div>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-3 text-center">
-          <link rel="icon" type="image/png" href="../assets/images/logoPlanilla.png"/>
-          <img src="../assets/images/logoPlanilla.png" alt="">
-        </div>
-        <div class="col-md-7 text-center">
-          <h4>ASOCIACION BOMBEROS VOLUNTARIOS DE TRENQUE LAUQUEN</h4>
-          <div class="text-center separate">
-            <h5>Parte de Asistencia del Personal del cuerpo activo</h5>
+      <div class="panel-body form-horizontal">
+        <div class="form-group col-sm-3">
+          {{ Form::label('day', 'Day:',['class' => 'control-label col-sm-3']) }}
+          <div class="col-sm-9">
+            {{Form::text('day', \Carbon\Carbon::now()->format('d'), ['class' => 'form-control','id'=>'day'])}}
           </div>
         </div>
-        <table class="text-center table table-bordered">
-          <tr>
-            <td>Leg.</td>
-            <td>Jerarquia</td>
-            <td>Apellido y Nombre</td>
-            <td>Cifra y Firma</td>
-          </tr>
-        </table>
-        <table class="text-center table table-bordered">
-          <tr>
-            <td>Jefe de Servicio</td>
-            <td>Oficial de Servicio</td>
-            <td>Jefe de Cuerpo</td>
-          </tr>
-          <tr>
-            <td><br><br><br></td>
-            <td><br><br><br></td>
-            <td><br><br><br></td>
-          </tr>
-        </table>
-        <div class="col-md-12">
-          <p>PRESENTE: 01:&#160;GUARDIA&#160;-&#160;COMANDO&#160;-&#160;CEREMONIAL&#160;-&#160;ASISTENCIA PROGRAMADA<br>
-            02:&#160;EN PRIMERA DOTACION&#160;-&#160;03: EN OTRA DOTACION&#160;-&#160;04: EN EL CUARTEL&#160;-&#160;05: EN COMISION
-          </p>
-          <p>AUSENTES: 06:&#160;LICENCIA&#160;-&#160;07: ENFERMO&#160;-&#160;08: CUMPLIENDO CASTIGO&#160;-&#160;09: CON AVISO&#160;-&#160; 10: SIN AVISO <br>
-            EN SERVICIOS ACCIDENTALES: LOS AUSENTES SE DEBEN TACHAR CON UNA RAYA.
-          </p>
+        <div class="form-group col-sm-3">
+          {{ Form::label('month', 'Month:',['class' => 'control-label col-sm-3']) }}
+          <div class="col-sm-9">
+            {{Form::select('month', config('selects.meses'),\Carbon\Carbon::now()->format('m'), ['class' => 'form-control','id' => 'month'])}}
+          </div>
         </div>
-        <div class="col-md-12">
-          <h5>OBSERVACIONES:</h5>
-          <table class="table table-bordered">
-            <tr>
-              <td></td>
-            </tr>
-          </table>
-          <div class="col-md-12">
-            <button type="submit" id="print" class="btn btn-default pull-left">
-              <i class=" glyphicon glyphicon-print"></i> Imprimir
-            </button>
+        <div class="form-group col-sm-3">
+          {{ Form::label('year', 'Year:',['class' => 'control-label col-sm-3']) }}
+          <div class="col-sm-9">
+            {{Form::text('year', \Carbon\Carbon::now()->format('Y'), ['class' => 'form-control','id'=>'year'])}}
+          </div>
+        </div>
+        <div class="container-fluid" id="planillaReporteServicio">
+          <div class="row">
+            <div class="col-xs-12 col-md-12 table-responsive">
+              <div class="" id="tablaReporteServicio">
+
+              </div>
+              <div class="col-xs-12 col-md-12">
+                <p>01: Incendio &#160;&#160;&#160; - &#160; 02: Auxilio &#160;&#160;&#160; - &#160; 03: Especiales &#160;&#160;&#160; - &#160; 04: Desastre &#160;&#160;&#160; - &#160; 05: Colaboración &#160;&#160;&#160; - &#160; 06: Guardias <br>
+                  07: Comando &#160;&#160;&#160; - &#160; 08: Tecnico &#160;&#160;&#160; - &#160; 09: Ceremonial &#160;&#160;&#160; - &#160; 10: Productos Quimicos &#160;&#160;&#160; - &#160; 11: Forestales
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-2 text-center">
+                <link rel="icon" type="image/png" href="../assets/images/logoPlanilla.png"/>
+                <img src="../assets/images/logoPlanilla.png" alt="">
+              </div>
+              <div class="col-md-8 text-center">
+                <h4>ASOCIACION BOMBEROS VOLUNTARIOS DE TRENQUE LAUQUEN</h4>
+                <div class="text-center separate">
+                  <h5>Parte de Asistencia del Personal del cuerpo activo</h5>
+                </div>
+              </div>
+              <table class="text-center table table-bordered">
+                <tr>
+                  <td>Leg.</td>
+                  <td>Jerarquia</td>
+                  <td>Apellido y Nombre</td>
+                  <td>Cifra y Firma</td>
+                </tr>
+              </table>
+              <table class="text-center table table-bordered">
+                <tr>
+                  <td>Jefe de Servicio</td>
+                  <td>Oficial de Servicio</td>
+                  <td>Jefe de Cuerpo</td>
+                </tr>
+                <tr>
+                  <td><br><br><br></td>
+                  <td><br><br><br></td>
+                  <td><br><br><br></td>
+                </tr>
+              </table>
+              <div class="col-md-12">
+                <p>PRESENTE: 01:&#160;GUARDIA&#160;-&#160;COMANDO&#160;-&#160;CEREMONIAL&#160;-&#160;ASISTENCIA PROGRAMADA<br>
+                  02:&#160;EN PRIMERA DOTACION&#160;-&#160;03: EN OTRA DOTACION&#160;-&#160;04: EN EL CUARTEL&#160;-&#160;05: EN COMISION
+                </p>
+                <p>AUSENTES: 06:&#160;LICENCIA&#160;-&#160;07: ENFERMO&#160;-&#160;08: CUMPLIENDO CASTIGO&#160;-&#160;09: CON AVISO&#160;-&#160; 10: SIN AVISO <br>
+                  EN SERVICIOS ACCIDENTALES: LOS AUSENTES SE DEBEN TACHAR CON UNA RAYA.
+                </p>
+              </div>
+              <div class="col-md-12">
+                <h5>OBSERVACIONES:</h5>
+                <table class="table table-bordered">
+                  <tr>
+                    <td></td>
+                  </tr>
+                </table>
+                <div class="col-md-12">
+                  <button type="submit" id="print" class="btn btn-default pull-left">
+                    <i class=" glyphicon glyphicon-print"></i> Imprimir
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </article>
 @endsection
 
 @section('js')
   {!! Html::script('assets/js/imprimirreporte.js') !!}
+  {!! Html::script('assets/js/ajaxserviciodia.js') !!}
 @endsection
