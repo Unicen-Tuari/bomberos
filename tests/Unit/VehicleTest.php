058f1dcac5e4107e2d45df3ddc9d7ed2cdf63cbd
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-10 00:53:06 -0300

    Empezando con crear los archivos para los unitTest

---
+++ tests/Unit/VehicleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class VehicleTest extends TestCase
+{
+    public function testScopePatente()
+    {
+        //setUp
+        
+    }
+}
