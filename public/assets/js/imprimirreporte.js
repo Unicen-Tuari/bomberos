775953890263decfc7df4087a8460dddf03dd2a9
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-10 10:34:01 -0300

    Ahora se puede imprimir perfectamente conservando los estilos

--- public/assets/js/imprimirreporte.js
+++ public/assets/js/imprimirreporte.js
@@ -1,3 +1,7 @@
 $('#print').on("click",function(){
+	let planilla = document.getElementById('planillaReporteServicio').innerHTML;
+	let contenidoOriginal = document.body.innerHTML;
+	document.body.innerHTML = planilla;
 	window.print();
+	document.body.innerHTML = contenidoOriginal;
 });
