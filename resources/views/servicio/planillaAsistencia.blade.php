aa057d25d2fc2132881bee88c04360d78b65bbc3
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-11 12:14:13 -0300

    Contiene la planilla de asistencia

--- resources/views/servicio/planillaAsistencia.blade.php
+++ resources/views/servicio/planillaAsistencia.blade.php
@@ -3,6 +3,16 @@
 		<td>Leg.</td>
 		<td>Jerarquia</td>
 		<td>Apellido y Nombre</td>
-		<td>Cifra y Firma</td>
+		<td>Cifra</td>
+		<td>Firma</td>
 	</tr>
+	@foreach($bomberos as $clave => $bombero)
+	<tr>
+		<td>{{$bombero->nro_legajo}}</td>
+		<td>{{config('selects.jerarquia')[$bombero->jerarquia]}}</td>
+		<td>{{$bombero->apellido}}, {{$bombero->nombre}}</td>
+		<td>&#160;&#160;&#160;</td>
+		<td>&#160;&#160;&#160;</td>
+	</tr>
+@endforeach
 </table>
