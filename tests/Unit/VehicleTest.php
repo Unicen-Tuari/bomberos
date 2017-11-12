80c2e1a44b40bc3b458f712bc224bb5dd3fc64a6
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-11 22:35:37 -0300

    Primer unit test de patente vehiculo

--- tests/Unit/VehicleTest.php
+++ tests/Unit/VehicleTest.php
@@ -3,13 +3,18 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use App\Vehiculo;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class VehicleTest extends TestCase
 {
     public function testScopePatente()
     {
-        //setUp
-        
+      //setUp
+       $vehiculo = factory(Vehiculo::class)->create();
+      //Modelo a probar
+       $vehiculoModel = Vehiculo::movil($vehiculo->patente)->first();
+      //Assert
+       $this->assertEquals($vehiculoModel->patente, $vehiculo->patente);
     }
 }
