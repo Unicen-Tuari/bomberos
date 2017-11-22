3635161d3206764f5da07dd1c9b9c234e69ed063
gaston <gaston0403@gmail.com>
2017-11-22 18:00:56 -0300

    hice los unit Test de los scopes de usuarios,en este caso para el Id y Nombre o Apellido

---
+++ tests/Unit/UsuarioTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Tests\Unit;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class ServiceTest extends TestCase
+{
+    public function testScopeId()
+    {
+        $newUser = factory(User::class)->create();
+        $newUserModel = User::Id($newUser->id);
+        $this->assertEquals($newUserModel->first()->id,$newUser->id);
+    }
+
+    public function testScopeNombre()
+    {
+        $newUser = factory(User::class)->create();
+        $newUserModel = User::nombre($newUser->nombre);
+        $this->assertEquals($newUserModel->first()->nombre,$newUser->nombre);
+    }
+}
