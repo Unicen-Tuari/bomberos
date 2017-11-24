5b1bba9acd75128055783ea930b218fabc3de6de
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-24 16:49:19 -0300

    Se crearon mas vehiculos en la base para que se vea mejor que funciona el filtrado

--- tests/Unit/VehicleTest.php
+++ tests/Unit/VehicleTest.php
@@ -11,6 +11,7 @@ class VehicleTest extends TestCase
     public function testScopePatente()
     {
        $vehiculo = factory(Vehiculo::class)->create();
+       $vehiculo2 = factory(Vehiculo::class)->create();
        $vehiculoModel = Vehiculo::patente($vehiculo->patente);
        $this->assertEquals($vehiculoModel->first()->patente, $vehiculo->patente);
        $this->assertEquals($vehiculoModel->count(),1);
@@ -19,6 +20,7 @@ class VehicleTest extends TestCase
     public function testScopeMovil()
     {
       $vehiculo = factory(Vehiculo::class)->create();
+      $vehiculo2 = factory(Vehiculo::class)->create();
       $vehiculoModel = Vehiculo::movil($vehiculo->num_movil);
       $this->assertEquals($vehiculoModel->first()->num_movil, $vehiculo->num_movil);
       $this->assertEquals($vehiculoModel->count(),1);
@@ -27,6 +29,7 @@ class VehicleTest extends TestCase
     public function testScopeEstado()
     {
       $vehiculo = factory(Vehiculo::class)->create(['estado' => 2]);
+      $vehiculo2 = factory(Vehiculo::class)->create(['estado' => 1]);
       $vehiculoModel = Vehiculo::estado($vehiculo->estado);
       $this->assertEquals($vehiculoModel->first()->estado, $vehiculo->estado);
       $this->assertEquals($vehiculoModel->count(),1);
