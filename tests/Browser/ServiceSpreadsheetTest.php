748ef0cd064bfcace1cdb19f5b88a8d8dfd42984
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-24 19:14:25 -0300

    Pasaron los test

--- tests/Browser/ServiceSpreadsheetTest.php
+++ tests/Browser/ServiceSpreadsheetTest.php
@@ -7,27 +7,18 @@ use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Servicio;
+use App\User;
 
 class ServiceSpreadsheetTest extends DuskTestCase
 {
-  public function testSeeIconList()
-  {
-    $this->browse(function (Browser $browser) {
-      $servicio->factory(Servicio::class)->create();
-      $browser->loginAs(User::find(1))
-              ->visit('/servicio')
-              ->assertSee('.glyphicon-list-alt');
-    });
-  }
-
-  public function testSeePlanilla()
-  {
-    $this->browse(function (browser $browser){
-			$servicio->factory(Servicio::class)->create();
-      $browser->loginAs(User::find(1))
-              ->visit('/servicio')
-							->click('glyphicon-list-alt')
-              ->assertDontSee('No hay datos cargados');
-    });
-  }
+	public function testSeePlanilla()
+	{
+		$this->browse(function (browser $browser){
+			$servicio = factory(Servicio::class)->create();
+			$browser->loginAs(User::find(1))
+			->visit('/servicio')
+			->click('.glyphicon-list-alt')
+			->assertDontSee('No hay datos cargados');
+		});
+	}
 }
