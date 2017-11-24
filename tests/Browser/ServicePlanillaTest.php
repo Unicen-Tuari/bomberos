6fe417ad0d66e3b7e896048e1084fb67b871a03a
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-24 17:09:06 -0300

    Se creo el test de verPlanilla

---
+++ tests/Browser/ServicePlanillaTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Database\Seeder;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Servicio;
+
+class VehicleTest extends DuskTestCase
+{
+  public function testSeeIconList()
+  {
+    $this->browse(function (Browser $browser) {
+      $servicio->factory(Servicio::class)->create();
+      $browser->loginAs(User::find(1))
+              ->visit('/servicio')
+              ->assertSee('.glyphicon-list-alt');
+    });
+  }
+
+  public function testSeePlanilla()
+  {
+    $this->browse(function (browser $browser){
+			$servicio->factory(Servicio::class)->create();
+      $browser->loginAs(User::find(1))
+              ->visit('/servicio')
+							->click('glyphicon-list-alt')
+              ->assertDontSee('No hay datos cargados');
+    });
+  }
+}
