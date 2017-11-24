823370c9abfc5eb82056a5f3c4b8c84ebf599ed0
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-24 16:50:56 -0300

    Refactor

--- tests/Unit/VehicleTest.php
+++ tests/Unit/VehicleTest.php
@@ -8,30 +8,30 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class VehicleTest extends TestCase
 {
-    public function testScopePatente()
-    {
-       $vehiculo = factory(Vehiculo::class)->create();
-       $vehiculo2 = factory(Vehiculo::class)->create();
-       $vehiculoModel = Vehiculo::patente($vehiculo->patente);
-       $this->assertEquals($vehiculoModel->first()->patente, $vehiculo->patente);
-       $this->assertEquals($vehiculoModel->count(),1);
-    }
+  public function testScopePatente()
+  {
+    $vehiculo = factory(Vehiculo::class)->create();
+    $vehiculo2 = factory(Vehiculo::class)->create();
+    $vehiculoModel = Vehiculo::patente($vehiculo->patente);
+    $this->assertEquals($vehiculoModel->first()->patente, $vehiculo->patente);
+    $this->assertEquals($vehiculoModel->count(),1);
+  }
 
-    public function testScopeMovil()
-    {
-      $vehiculo = factory(Vehiculo::class)->create();
-      $vehiculo2 = factory(Vehiculo::class)->create();
-      $vehiculoModel = Vehiculo::movil($vehiculo->num_movil);
-      $this->assertEquals($vehiculoModel->first()->num_movil, $vehiculo->num_movil);
-      $this->assertEquals($vehiculoModel->count(),1);
-    }
+  public function testScopeMovil()
+  {
+    $vehiculo = factory(Vehiculo::class)->create();
+    $vehiculo2 = factory(Vehiculo::class)->create();
+    $vehiculoModel = Vehiculo::movil($vehiculo->num_movil);
+    $this->assertEquals($vehiculoModel->first()->num_movil, $vehiculo->num_movil);
+    $this->assertEquals($vehiculoModel->count(),1);
+  }
 
-    public function testScopeEstado()
-    {
-      $vehiculo = factory(Vehiculo::class)->create(['estado' => 2]);
-      $vehiculo2 = factory(Vehiculo::class)->create(['estado' => 1]);
-      $vehiculoModel = Vehiculo::estado($vehiculo->estado);
-      $this->assertEquals($vehiculoModel->first()->estado, $vehiculo->estado);
-      $this->assertEquals($vehiculoModel->count(),1);
-    }
+  public function testScopeEstado()
+  {
+    $vehiculo = factory(Vehiculo::class)->create(['estado' => 2]);
+    $vehiculo2 = factory(Vehiculo::class)->create(['estado' => 1]);
+    $vehiculoModel = Vehiculo::estado($vehiculo->estado);
+    $this->assertEquals($vehiculoModel->first()->estado, $vehiculo->estado);
+    $this->assertEquals($vehiculoModel->count(),1);
+  }
 }
