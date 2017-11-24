54ed3f15d180933694e53be7e191f3ffc18e0125
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-24 17:54:16 -0300

    TestVehiculo corregido y funcionando

--- tests/Browser/VehicleTest.php
+++ tests/Browser/VehicleTest.php
@@ -11,72 +11,61 @@ use App\User;
 
 class VehicleTest extends DuskTestCase
 {
-    private $vehiculo;
-    private $vehiculoEdit;
-
-  public function setUp()
-  {
-      parent::setUp();
-      $this->vehiculo = factory(Vehiculo::class)->make();
-      $this->vehiculoEdit = factory(Vehiculo::class)->make();
-  }
 
   public function testCreate()
   {
     $this->browse(function (Browser $browser) {
+      $this->vehiculo = factory(Vehiculo::class)->make();
       $patente = strtoupper($this->vehiculo->patente);
       $browser->loginAs(User::find(1))
-              ->visit('/vehiculo/create')
-              ->type('patente',$this->vehiculo->patente)
-              ->type('num_movil',$this->vehiculo->num_movil)
-              ->select('estado',$this->vehiculo->estado)
-              ->type('detalle',$this->vehiculo->detalle)
-              ->press('Registrar')
-              ->type('patente',$patente)
-              ->press('Buscar')
-              ->assertSee($this->vehiculo->num_movil);
+      ->visit('/vehiculo/create')
+      ->type('patente',$this->vehiculo->patente)
+      ->type('num_movil',$this->vehiculo->num_movil)
+      ->select('estado',$this->vehiculo->estado)
+      ->type('detalle',$this->vehiculo->detalle)
+      ->press('Registrar')
+      ->type('patente',$patente)
+      ->press('Buscar')
+      ->assertSee($this->vehiculo->num_movil);
     });
   }
 
   public function testUpdate()
   {
     $this->browse(function (browser $browser){
+      $this->vehiculo = factory(Vehiculo::class)->create();
+      $this->vehiculoEdit = factory(Vehiculo::class)->make();
       $patente = strtoupper($this->vehiculoEdit->patente);
       $browser->loginAs(User::find(1))
-              ->visit('/vehiculo')
-              ->click('.glyphicon-edit')
-              ->type('patente', $this->vehiculoEdit->patente)
-              ->type('num_movil', $this->vehiculoEdit->num_movil)
-              ->select('estado',$this->vehiculoEdit->estado)
-              ->type('detalle',$this->vehiculoEdit->detalle)
-              ->press('Editar')
-              ->type('patente', $patente)
-              ->press('Buscar')
-              ->assertSee($this->vehiculoEdit->num_movil);
+      ->visit('/vehiculo')
+      ->click('.glyphicon-edit')
+      ->type('patente', $this->vehiculoEdit->patente)
+      ->type('num_movil', $this->vehiculoEdit->num_movil)
+      ->select('estado',$this->vehiculoEdit->estado)
+      ->type('detalle',$this->vehiculoEdit->detalle)
+      ->press('Editar')
+      ->type('patente', $patente)
+      ->press('Buscar')
+      ->assertSee($this->vehiculoEdit->num_movil);
     });
   }
 
   public function testDelete()
   {
-     $this->browse(function(browser $browser){
-       $patente = strtoupper($this->vehiculo->patente);
-       $browser->loginAs(User::find(1))
-               ->visit('/vehiculo')
-               ->click('.glyphicon-edit')
-               ->type('patente',$this->vehiculo->patente)
-               ->press('Editar')
-               ->type('patente',$this->vehiculo->patente)
-               ->press('Buscar')
-               ->click('.glyphicon-trash')
-               ->type('patente',$patente)
-               ->press('Buscar')
-               ->assertDontSee($this->vehiculo->num_movil);
-     });
-   }
-
-  public function tearDown()
-  {
-   $this->vehiculoEdit->delete();
-   $this->vehiculo->delete();
+    $this->browse(function(browser $browser){
+      $this->vehiculo = factory(Vehiculo::class)->create();
+      $patente = strtoupper($this->vehiculo->patente);
+      $browser->loginAs(User::find(1))
+      ->visit('/vehiculo')
+      ->click('.glyphicon-edit')
+      ->type('patente',$this->vehiculo->patente)
+      ->press('Editar')
+      ->type('patente',$this->vehiculo->patente)
+      ->press('Buscar')
+      ->click('.glyphicon-trash')
+      ->type('patente',$patente)
+      ->press('Buscar')
+      ->assertDontSee($this->vehiculo->num_movil);
+    });
   }
 }
