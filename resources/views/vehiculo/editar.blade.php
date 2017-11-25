6b16227b019635529887cf6a7dcc9621d3aa7952
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-24 21:41:18 -0300

    Se modifico el form de editar vehiculo

--- resources/views/vehiculo/editar.blade.php
+++ resources/views/vehiculo/editar.blade.php
@@ -8,6 +8,68 @@
       <h4>Modificar vehiculo</h4>
     </div>
     <div class="panel-body">
+      <form class='form-horizontal' action="{{route('vehiculo.update', $vehiculo)}}" method="POST">
+        {{csrf_field()}}
+        {{method_field('PUT')}}
+        <div class="form-group {{ $errors->has('patente') ? ' has-error' : '' }}">
+          <label class="col-md-4 control-label" for="patente">Patente</label>
+          <div class="col-md-4">
+            <input class="form-control" type="text" name="patente" @if(old('patente')) value={{old('patente')}} @endif value="{{$vehiculo->patente}}">
+            @if ($errors->has('patente'))
+              <span class="help-block">
+                <strong>{{ $errors->first('patente') }} Usar: AAA 999 o AA 999 AA</strong>
+              </span>
+            @endif
+          </div>
+        </div>
+        <div class="form-group {{ $errors->has('num_movil') ? ' has-error' : '' }}">
+          <label class="col-md-4 control-label" for="num_movil">Numero de Movil</label>
+          <div class="col-md-4">
+            <input class="form-control" type="text" name="num_movil" @if(old('num_movil')) value={{old('num_movil')}} @endif value="{{$vehiculo->num_movil}}">
+            @if ($errors->has('num_movil'))
+              <span class="help-block">
+                <strong>{{ $errors->first('num_movil') }}</strong>
+              </span>
+            @endif
+          </div>
+        </div>
+        <div class="form-group {{ $errors->has('estado') ? ' has-error' : '' }}">
+          <label class="col-md-4 control-label" for="estado">Estado</label>
+          <div class="col-md-4">
+            <select class="form-control" name="estado">
+              @foreach(config('selects.estadovehiculo') as $key => $estadoVehiculo)
+                <option value="{{$key}}" @if ($key == old('estado')) value={{old('estado')}} selected="selected" @endif>{{$estadoVehiculo}}</option>
+                @endforeach
+              </select>
+              @if ($errors->has('estado'))
+                <span class="help-block">
+                  <strong>{{ $errors->first('estado') }}</strong>
+                </span>
+              @endif
+            </div>
+          </div>
+          <div class="form-group {{ $errors->has('detalle') ? ' has-error' : '' }}">
+            <label class="col-md-4 control-label" for="detalle">Detalle</label>
+            <div class="col-md-4">
+              <textarea class="form-control" name="detalle" cols="50" rows="8">{{$vehiculo->detalle}}</textarea>
+              @if ($errors->has('detalle'))
+                <span class="help-block">
+                  <strong>{{ $errors->first('detalle') }}</strong>
+                </span>
+              @endif
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-md-6 col-md-offset-4">
+              <button type="submit" class="btn btn-primary">
+                <i class=" glyphicon glyphicon-floppy-save"></i> Registrar
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    <div class="panel-body">
+      @php /*
       {!! Form::open([ 'route' => ['vehiculo.update', $vehiculo ], 'class' => 'form-horizontal', 'method' => 'PUT', 'files' => true]) !!}
 
         <div class="form-group {{ $errors->has('patente') ? ' has-error' : '' }}">
@@ -72,6 +134,7 @@
 
       {!! Form::close() !!}
     </div>
+    */@endphp
   </div>
 </article>
 @endsection
