32d1a83839e4819bead57f1f99afd397f2dec29b
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-11-24 21:50:29 -0300

    Refactor

--- resources/views/servicio/servicios.blade.php
+++ resources/views/servicio/servicios.blade.php
@@ -10,7 +10,7 @@
       </div>
       <div class="col-sm-12">
         <div class="col-md-10 col-sm-12 text-right" style="padding-top: 20px;">
-          <form class='form-horizontal' action ={{route('servicio.index')}} method = 'GET'>
+          <form class='form-horizontal' action={{route('servicio.index')}} method='GET'>
             <div class="col-sm-3">
               <select class="form-control" name="tipo_servicio">
                 <option value=0 >Tipo de Servicio</option>
@@ -36,7 +36,7 @@
                     </select>
                   </div>
                   <div class="col-sm-2">
-                    <input class="form-control" name="year" @if(old('year')) value= {{old('year')}} @endif placeholder={{\Carbon\Carbon::now()->format('Y')}}>
+                    <input class="form-control" name="year" @if(old('year')) value={{old('year')}} @endif placeholder={{\Carbon\Carbon::now()->format('Y')}}>
                     </div>
                     <div class="col-sm-1">
                       <button class="btn btn-primary" type="submit" name="Buscar">Buscar</button>
@@ -61,14 +61,14 @@
                           <td class="text-center"><a href="{{route('servicio.show', $servicio->id)}}">{{config('selects.tipoServicio')[$servicio->tipo_servicio_id]}}</a></td>
                           <td class="text-center">{{$servicio->direccion}}</td>
                           <td class="text-center">{{\Carbon\Carbon::parse($servicio->hora_regreso)->format('d/m/Y H:i:s')}}</td>
-                          <td class="text-center"><a class="glyphicon glyphicon-list-alt" href="{{ route('servicio.planilla',$servicio->id) }}"></a></td>
+                          <td class="text-center"><a class="glyphicon glyphicon-list-alt" href="{{route('servicio.planilla',$servicio->id)}}"></a></td>
                           @if (Auth::user()->admin)
                             <td class="text-center"><form class="form-horizontal" action={{route('servicio.destroy', $servicio->id)}} method="POST">
                               {{csrf_field()}}
                               {{method_field('DELETE')}}
                               <button type="submit" class="glyphicon glyphicon-trash"></button>
                             </form></td>
-                            <td class="text-center"><a class="glyphicon glyphicon-edit" href="{{ route('servicio.edit', $servicio->id) }}"></a></td>
+                            <td class="text-center"><a class="glyphicon glyphicon-edit" href="{{route('servicio.edit', $servicio->id)}}"></a></td>
                           @else
                             <td class="text-center" colspan="2">
                               <button type="submit" class="glyphicon glyphicon-ban-circle" title="Sin permisos para eliminar/modificar"></button>
