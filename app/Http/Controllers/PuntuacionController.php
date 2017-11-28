b391c56f9b2bb9267a0484a4b03fc033c96de67f
SSAAMMAA <facundo.samartino@gmail.com>
2017-11-28 18:41:44 -0300

    faltan editar algunos forms

--- app/Http/Controllers/PuntuacionController.php
+++ app/Http/Controllers/PuntuacionController.php
@@ -46,23 +46,23 @@ class PuntuacionController extends Controller
         return view('auth/alerta');
     }
 
-    public function puntuacionmes($mes,$año,$bombero)
+    public function puntuacionmes($month,$year,$bombero)
     {
         if(Auth::user()->admin){
-          $var=Variables::where('anio', '=', $año)->first();
+          $var=Variables::where('anio', '=', $year)->first();
           $mesactual=\Carbon\Carbon::now()->format('m');
-          $añoactual=\Carbon\Carbon::now()->format('Y');
-          if($año<$añoactual || ($año==$añoactual && $mes<$mesactual)){
+          $actualYear=\Carbon\Carbon::now()->format('Y');
+          if($year<$actualYear || ($year==$actualYear && $month<$mesactual)){
             $bombero=Bombero::find($bombero);
-            $dias=asistencia::select(\DB::raw('COUNT(*) as cant, id_bombero'))->whereYear('fecha_reunion','=',$año)->whereMonth('fecha_reunion','=',$mes)->groupBy('id_bombero')->get()->max('cant');
+            $dias=asistencia::select(\DB::raw('COUNT(*) as cant, id_bombero'))->whereYear('fecha_reunion','=',$year)->whereMonth('fecha_reunion','=',$month)->groupBy('id_bombero')->get()->max('cant');
             if ($dias==null) {
               $dias=0;
             }
-            $cantserv=Servicio::whereNotNull('hora_regreso')->where('tipo_alarma', 3)->whereYear('hora_alarma','=',$año)->whereMonth('hora_alarma','=',$mes)->count();
-            $cantguar=Servicio::whereNotNull('hora_regreso')->where('tipo_alarma','<', 3)->whereYear('hora_alarma','=',$año)->whereMonth('hora_alarma','=',$mes)->count();
-            $accid=$bombero->accidentales($mes,$año);
-            $guardia=$bombero->guardias($mes,$año);
-            $asistencia=$bombero->asistenciasmes($mes,$año);
+            $cantserv=Servicio::whereNotNull('hora_regreso')->where('tipo_alarma', 3)->whereYear('hora_alarma','=',$year)->whereMonth('hora_alarma','=',$month)->count();
+            $cantguar=Servicio::whereNotNull('hora_regreso')->where('tipo_alarma','<', 3)->whereYear('hora_alarma','=',$year)->whereMonth('hora_alarma','=',$month)->count();
+            $accid=$bombero->accidentales($month,$year);
+            $guardia=$bombero->guardias($month,$year);
+            $asistencia=$bombero->asistenciasmes($month,$year);
             $puntasis=0;
             if ($dias!=0) {
               $puntasis=($var->asistencia/$dias)*$asistencia;
@@ -80,43 +80,43 @@ class PuntuacionController extends Controller
                 $puntguar=$var->guardias-(5*($cantguar-$guardia));
             }
             return view('puntuacion/alta',
-            compact('bombero','cantserv','cantguar','mes','año','dias','accid','guardia','asistencia','puntasis','puntaccid','puntguar'));
+            compact('bombero','cantserv','cantguar','month','year','dias','accid','guardia','asistencia','puntasis','puntaccid','puntguar'));
           }
           return view('errors/aviso');
         }
         return view('auth/alerta');
     }
 
-    public function listar($mes,$año)
+    public function listar($month,$year)
     {
         $bomberos=Bombero::where('activo', 1)->get();
-        $dias=asistencia::select(\DB::raw('COUNT(*) as cant, id_bombero'))->whereYear('fecha_reunion','=',$año)->whereMonth('fecha_reunion','=',$mes)->groupBy('id_bombero')->get()->max('cant');
+        $dias=asistencia::select(\DB::raw('COUNT(*) as cant, id_bombero'))->whereYear('fecha_reunion','=',$year)->whereMonth('fecha_reunion','=',$month)->groupBy('id_bombero')->get()->max('cant');
         if ($dias==null) {
           $dias=0;
         }
-        $cantserv=Servicio::whereNotNull('hora_regreso')->where('tipo_alarma', 3)->whereYear('hora_alarma','=',$año)->whereMonth('hora_alarma','=',$mes)->count();
-        $cantguar=Servicio::whereNotNull('hora_regreso')->where('tipo_alarma','<', 3)->whereYear('hora_alarma','=',$año)->whereMonth('hora_alarma','=',$mes)->count();
-        return view('puntuacion/tabla',compact('bomberos','cantserv','cantguar','mes','año','dias'));
+        $cantserv=Servicio::whereNotNull('hora_regreso')->where('tipo_alarma', 3)->whereYear('hora_alarma','=',$year)->whereMonth('hora_alarma','=',$month)->count();
+        $cantguar=Servicio::whereNotNull('hora_regreso')->where('tipo_alarma','<', 3)->whereYear('hora_alarma','=',$year)->whereMonth('hora_alarma','=',$month)->count();
+        return view('puntuacion/tabla',compact('bomberos','cantserv','cantguar','month','year','dias'));
     }
 
     public function store(Request $request)
     {
         if(Auth::user()->admin){
           $date=$request->all();
-          $fecha=\Carbon\Carbon::parse($date['año'].'-'.$date['mes'].'-'.'1');
+          $fecha=\Carbon\Carbon::parse($date['year'].'-'.$date['month'].'-'.'1');
           $date['fecha']=$fecha;
-          unset($date['mes'],$date['año']);//elimino los elemnetos qeu no machean con la tabla
+          unset($date['month'],$date['year']);//elimino los elemnetos qeu no machean con la tabla
           Puntuacion::create($date);
         }
     }
 
-    public function bomberos($mes,$año)
+    public function bomberos($month,$year)
     {
       $mesactual=\Carbon\Carbon::now()->format('m');
-      $añoactual=\Carbon\Carbon::now()->format('Y');
-      if($año<$añoactual || ($año==$añoactual && $mes<$mesactual)){
+      $actualYear=\Carbon\Carbon::now()->format('Y');
+      if($year<$actualYear || ($year==$actualYear && $month<$mesactual)){
         $bomberos=Bombero::where('activo', 1)->get();
-        return view('puntuacion/puntuacionmes',compact('bomberos','mes','año'));
+        return view('puntuacion/puntuacionmes',compact('bomberos','month','year'));
       }
       return view('errors/aviso');
     }
@@ -130,22 +130,22 @@ class PuntuacionController extends Controller
       if(Auth::user()->admin){
         $var=Variables::first();
         $puntuacion=Puntuacion::find($id);
-        $mes=\Carbon\Carbon::parse($puntuacion->fecha)->format('m');
-        $año=\Carbon\Carbon::parse($puntuacion->fecha)->format('Y');
+        $month=\Carbon\Carbon::parse($puntuacion->fecha)->format('m');
+        $year=\Carbon\Carbon::parse($puntuacion->fecha)->format('Y');
         $dias=asistencia::select(\DB::raw('COUNT(*) as cant, id_bombero'))
-          ->whereYear('fecha_reunion','=',$año)->whereMonth('fecha_reunion','=',$mes)
+          ->whereYear('fecha_reunion','=',$year)->whereMonth('fecha_reunion','=',$month)
           ->groupBy('id_bombero')->get()->max('cant');
         if ($dias==null) {
           $dias=0;
         }
-        $cantserv=Servicio::where('tipo_alarma', 3)->whereYear('hora_alarma','=',$año)
-          ->whereMonth('hora_alarma','=',$mes)->count();
-        $cantguar=Servicio::where('tipo_alarma','<', 3)->whereYear('hora_alarma','=',$año)
-          ->whereMonth('hora_alarma','=',$mes)->count();
+        $cantserv=Servicio::where('tipo_alarma', 3)->whereYear('hora_alarma','=',$year)
+          ->whereMonth('hora_alarma','=',$month)->count();
+        $cantguar=Servicio::where('tipo_alarma','<', 3)->whereYear('hora_alarma','=',$year)
+          ->whereMonth('hora_alarma','=',$month)->count();
 
-        $accid=$puntuacion->bombero->accidentales($mes,$año);
-        $guardia=$puntuacion->bombero->guardias($mes,$año);
-        $asistencia=$puntuacion->bombero->asistenciasmes($mes,$año);
+        $accid=$puntuacion->bombero->accidentales($month,$year);
+        $guardia=$puntuacion->bombero->guardias($month,$year);
+        $asistencia=$puntuacion->bombero->asistenciasmes($month,$year);
         $puntasis=0;
         if ($dias!=0) {
           $puntasis=($var->asistencia/$dias)*$asistencia;
--- resources/views/material/info.blade.php
+++ resources/views/material/info.blade.php
@@ -1,66 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
-<article class="col-sm-12">
-  <div class="panel panel-default">
-    <div id="breadcrumb" class="panel-heading">
-      <span class="fa fa-edit" aria-hidden="true"></span>
-      <h4>Material {{$material->nombre}} {{$material->id}}</h4>
-    </div>
-    <div class="panel-body form-horizontal">
-      <div class="form-group">
-        {!! Form::label('nombre', 'Nombre',['class' => 'col-md-4 control-label']) !!}
-        <div class="col-md-6">
+  <article class="col-sm-12">
+    <div class="panel panel-default">
+      <div id="breadcrumb" class="panel-heading">
+        <span class="fa fa-edit" aria-hidden="true"></span>
+        <h4>Material {{$material->nombre}} {{$material->id}}</h4>
+      </div>
+      <div class="panel-body form-horizontal">
+        <div class="form-group">
+          <label for="nombre" class="col-md-4 control-label">Nombre: </label>
+          <div class="col-md-6">
             {!! Form::text('nombre', $material->nombre, ['class' => 'form-control' ,'disabled']) !!}
+          </div>
         </div>
-      </div>
 
-      <div class="form-group">
-        {!! Form::label('vehiculo_id', 'Esta en el vehiculo: ',['class' => 'col-md-4 control-label']) !!}
-        <div class="col-md-2">
-          {{Form::select('vehiculo_id',$vehiculos, $material->vehiculo_id, ['class' => 'form-control','disabled'])}}
-        </div>
-      </div>
-
-      <div class="form-group">
-        {!! Form::label('rubro', 'Rubro: ',['class' => 'col-md-4 control-label']) !!}
-        <div class="col-md-2">
-          {{Form::select('rubro',config('selects.rubro'), $material->rubro, ['class' => 'form-control','disabled'])}}
-        </div>
-      </div>
-
-      <div class="form-group">
-        <div class="">
-          {!! Form::label('mantenimiento', "Mantenimiento", ['class' => 'col-md-4 control-label']) !!}
+        <div class="form-group">
+          <label for="vehiculo_id" class="col-md-4 control-label">Esta en el vehiculo: </label>
           <div class="col-md-2">
+            {{Form::select('vehiculo_id',$vehiculos, $material->vehiculo_id, ['class' => 'form-control','disabled'])}}
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="rubro" class="col-md-4 control-label">Rubro: </label>
+          <div class="col-md-2">
+            {{Form::select('rubro',config('selects.rubro'), $material->rubro, ['class' => 'form-control','disabled'])}}
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="">
+            <label for="mantenimiento" class="col-md-4 control-label">Mantenimiento: </label>
+            <div class="col-md-2">
               {!! Form::checkbox('mantenimiento', 1,$material->mantenimiento,['disabled']) !!}
+            </div>
+          </div>
+
+          <div>
+            <label for="reparado" class="col-md-4 control-label">Veces reparado: </label>
+            <div class="col-sm-1 ">
+              {!! Form::text('reparado', $material->reparado,['class' => 'form-control','disabled' => 'disabled']) !!}
+            </div>
           </div>
         </div>
 
-        <div>
-          {!! Form::label('reparado', "Veces reparado", ['class' => 'col-md-2 control-label']) !!}
-          <div class="col-sm-1 ">
-            {!! Form::text('reparado', $material->reparado,['class' => 'form-control','disabled' => 'disabled']) !!}
-          </div>
-        </div>
-      </div>
-
-      <div class="form-group">
-        {!! Form::label('detalle', 'Detalle',['class' => 'col-sm-4 control-label']) !!}
-        <div class="col-sm-6">
+        <div class="form-group">
+          <label for="detalle" class="col-md-4 control-label">Detalle: </label>
+          <div class="col-sm-6">
             {!! Form::textarea('detalle', $material->detalle, ['class' => 'form-control' , 'rows' => '8','disabled']) !!}
+          </div>
         </div>
-      </div>
 
-      <div class="form-group">
-        <div class=" col-sm-3 col-sm-offset-6">
-          <a href="{{route('material.index')}}" class="">
-          <button type="submit" class="btn btn-primary glyphicon glyphicon-ok">
-          Volver</button></a>
+        <div class="form-group">
+          <div class=" col-sm-3 col-sm-offset-6">
+            <a href="{{route('material.index')}}" class="">
+              <button type="submit" class="btn btn-primary glyphicon glyphicon-ok">
+                Volver</button></a>
+              </div>
+            </div>
+
+          </div>
         </div>
-      </div>
-
-    </div>
-  </div>
-</article>
-@endsection
+      </article>
+    @endsection
