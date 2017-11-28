bf3d8a84906ca6b03247170947771c4277219fab
gaston <gaston0403@gmail.com>
2017-11-28 19:36:30 -0300

    se agregaron test

--- tests/Unit/BomberoTest.php
+++ tests/Unit/BomberoTest.php
@@ -8,16 +8,29 @@ class BomberoTest extends TestCase
 {
     public function testScopeNombre()
     {
-        $newBombero = factory(Bombero::class)->create();
+        $newBombero = factory(Bombero::class)->create(['nombre'=>'ruben']);
+        $newBombero2 = factory(Bombero::class)->create(['nombre'=>'dario']);
         $newBomberoModel = Bombero::Nombre($newBombero->nombre);
         $this->assertEquals($newBomberoModel->first()->nombre,$newBombero->nombre);
+        $this->assertNotEquals($newBomberoModel->first()->nombre,$newBombero2->nombre);
     }
 
     public function testScopeLegajo()
     {
-        $newBombero = factory(Bombero::class)->create();
-        $newBomberoModel = Bombero::Legajo($newBombero->Legajo);
-        $this->assertEquals($newBomberoModel->first()->legajo,$newBombero->legajo);
+        $newBombero = factory(Bombero::class)->create(['nro_legajo'=>'849302']);
+        $newBombero2 = factory(Bombero::class)->create(['nro_legajo'=>'632818']);
+        $newBomberoModel = Bombero::Legajo($newBombero->nro_legajo);
+        $this->assertEquals($newBomberoModel->first()->nro_legajo,$newBombero->nro_legajo);
+        $this->assertNotEquals($newBomberoModel->first()->nro_legajo,$newBombero2->nro_legajo);
+    }
+
+    public function testScopeJerarquia()
+    {
+        $newBombero = factory(Bombero::class)->create(['jerarquia'=>'3']);
+        $newBombero2 = factory(Bombero::class)->create(['jerarquia'=>'1']);
+        $newBomberoModel = Bombero::Jerarquia($newBombero->jerarquia);
+        $this->assertEquals($newBomberoModel->first()->jerarquia,$newBombero->jerarquia);
+        $this->assertNotEquals($newBomberoModel->first()->jerarquia,$newBombero2->jerarquia);
     }
 
     // public function testScopeLegajo()
@@ -27,11 +40,6 @@ class BomberoTest extends TestCase
     //     $this->assertEquals($newBomberoModel->first()->legajo,$newBombero->legajo);
     // }
 
-    public function testScopeJerarquia()
-    {
-        $newBombero = factory(Bombero::class)->create();
-        $newBomberoModel = Bombero::Jerarquia($newBombero->jerarquia);
-        $this->assertEquals($newBomberoModel->first()->Jerarquia,$newBombero->jerarquia);
-    }
+
 
 }
