ec659c4e8aa3f81373ae6f9556012749ae2f7b1d
Emmanuel Flores <emmanuelflores2010@gmail.com>
2017-11-29 20:53:20 -0300

    Funciones para que pase el unit test reemplazo (#192)

--- app/Reemplazo.php
+++ app/Reemplazo.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reemplazo extends Model
 {
-    //
+  protected $table = 'reemplazos';
+  protected $fillable = [
+    'id_bombero','id_bombero_reemplazo','fecha_inicio','fecha_fin','razon'
+  ];
+
+  public static function getActivos()
+  {
+    $fecha = date('Y-m-d H:i:s');
+    return Reemplazo::whereDate('fecha_fin', '>', $fecha)->get();
+  }
+
+  public static function getTerminados()
+  {
+    $fecha = date('Y-m-d H:i:s');
+    return Reemplazo::whereDate('fecha_fin', '<', $fecha)->get();
+  }
 }
--- tests/Unit/ReemplazoTest.php
+++ tests/Unit/ReemplazoTest.php
@@ -9,22 +9,27 @@ use App\Bombero;
 
 class ReemplazoTest extends TestCase
 {
-    public function testReemplazosActivos()
-    {
-        $reemplazo = factory(Reemplazo::class)->create();
-        $reemplazo2 = factory(Reemplazo::class)->create();
-        $reemplazosActivos = Reemplazo::reemplazosActivos();
-        $this->assertEquals($reemplazosActivos->count(),2);
-    }
-
-    public function testReemplazosTerminados()
+    public function testGetActivos()
     {
         $fecha = date('Y-m-d H:i:s');
         $fechaFin = strtotime('-2day' , strtotime($fecha));
         $fechaFin = date('Y-m-d H:i:s' , $fechaFin);
-        $reemplazo = factory(Reemplazo::class)->create(['fecha_fin'=>$fechaFin]);
-        $reemplazo2 = factory(Reemplazo::class)->create(['fecha_fin'=>$fechaFin]);
-        $reemplazosTerminados = Reemplazo::reemplazosTerminados();
+        $reemplazoActivo = factory(Reemplazo::class)->create();
+        $reemplazoActivo2 = factory(Reemplazo::class)->create();
+        $reemplazoTerminado = factory(Reemplazo::class)->create(['fecha_fin'=>$fechaFin]);
+        $reemplazosActivos = Reemplazo::getActivos();
+        $this->assertEquals($reemplazosActivos->count(),2);
+    }
+
+    public function testGetTerminados()
+    {
+        $fecha = date('Y-m-d H:i:s');
+        $fechaFin = strtotime('-2day' , strtotime($fecha));
+        $fechaFin = date('Y-m-d H:i:s' , $fechaFin);
+        $reemplazoTerminado = factory(Reemplazo::class)->create(['fecha_fin'=>$fechaFin]);
+        $reemplazoTerminado2 = factory(Reemplazo::class)->create(['fecha_fin'=>$fechaFin]);
+        $reemplazoActivo = factory(Reemplazo::class)->create();
+        $reemplazosTerminados = Reemplazo::getTerminados();
         $this->assertEquals($reemplazosTerminados->count(),2);
     }
 }
