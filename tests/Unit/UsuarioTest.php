9ecaab4edf5dfd9394f2735d8a30dc1ddf3759a1
gaston <gaston0403@gmail.com>
2017-11-30 16:42:01 -0300

    refactory, se controla que la funcion devuelva un solo usuario

--- tests/Unit/UsuarioTest.php
+++ tests/Unit/UsuarioTest.php
@@ -9,13 +9,17 @@ class UsuarioTest extends TestCase
     public function testScopeId()
     {
         $newUser = factory(User::class)->create();
+        $newUser2 = factory(User::class)->create();
         $newUserModel = User::Id($newUser->id);
         $this->assertEquals($newUserModel->first()->id,$newUser->id);
+        $this->assertEquals($newUserModel->count(),1);
     }
     public function testScopeNombre()
     {
         $newUser = factory(User::class)->create();
+        $newUser2 = factory(User::class)->create();
         $newUserModel = User::nombre($newUser->nombre);
         $this->assertEquals($newUserModel->first()->nombre,$newUser->nombre);
+        $this->assertEquals($newUserModel->count(),1);
     }
 }
