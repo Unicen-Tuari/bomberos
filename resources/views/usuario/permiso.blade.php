376bc48a4165dc5f9fef3d21bca3003a85d0d03e
gaston <gaston0403@gmail.com>
2017-11-30 15:56:25 -0300

    refactory. acomodando espacios

--- resources/views/usuario/permiso.blade.php
+++ resources/views/usuario/permiso.blade.php
@@ -16,14 +16,12 @@
           @foreach($usuarios as $usuario)
             <div class="col-lg-6 col-md-12 control-label">
               <label class="col-lg-7 col-md-5 control-label">  {{$usuario->nombre}} {{$usuario->apellido}}  / {{$usuario->usuario}} </label>
-
               <div class="col-sm-4">
                 <input type="checkbox" name="usuario" class="toggle btn btn-success" @if ($usuario->admin) checked @endif data-toggle="toggle" data-off="Usuario" data-on="Administrador" data-onstyle="success" data-offstyle="danger">
               </div>
             </div>
           @endforeach
         </div>
-
         <div class="form-group col-sm-7">
             <button type="submit" class="btn btn-primary pull-right">
                 <i class="glyphicon glyphicon-bell"> Finalizar</i>
