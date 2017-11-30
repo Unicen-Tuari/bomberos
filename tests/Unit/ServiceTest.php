55c469205b950444b1cd0db34509bac2e452132c
JerePrz <jereprz94>
2017-11-29 23:05:51 -0300

    correccion lineas de mas y borrado de comentarios close #174

--- tests/Unit/ServiceTest.php
+++ tests/Unit/ServiceTest.php
@@ -13,12 +13,11 @@ class ServiceTest extends TestCase
 
     public function testScopeTipo()
     {
-        //Setup
         $service = factory(Servicio::class)->create(['tipo_servicio_id'=>1]);
         $service1 = factory(Servicio::class)->create(['tipo_servicio_id'=>2]);
-        //Modelo a probar
+
         $services = Servicio::tipo($service->tipo_servicio_id);
-        //Assert
+        
         $this->assertEquals($services->first()->id,$service->id);
         $this->assertEquals($services->first()->tipo_servicio_id,$service->tipo_servicio_id);
         $this->assertEquals($services->count(),1);
@@ -26,12 +25,11 @@ class ServiceTest extends TestCase
 
     public function testScopeTipoAlarma()
     {
-        //Setup
         $service = factory(Servicio::class)->create(['tipo_alarma'=>1]);
         $service1 = factory(Servicio::class)->create(['tipo_alarma'=>2]);
-        //Modelo a probar
+
         $services = Servicio::tipoAlarma($service->tipo_alarma);
-        //Assert
+
         $this->assertEquals($services->first()->id,$service->id);
         $this->assertEquals($services->first()->tipo_alarma,$service->tipo_alarma);
         $this->assertEquals($services->count(),1);
@@ -39,21 +37,20 @@ class ServiceTest extends TestCase
 
     public function testScopeFecha()
     {
-        //Setup
         $horaRegreso1 = new DateTime("2015-11-01 00:00:00", new DateTimeZone("America/New_York"));
         $horaRegreso2 = new DateTime("2016-12-01 00:00:00", new DateTimeZone("America/New_York"));
         $horaRegreso3 = new DateTime("2013-12-01 00:00:00", new DateTimeZone("America/New_York"));
-
 
         $service1 = factory(Servicio::class)->create(['hora_regreso'=>$horaRegreso1->format("Y-m-d H:i:s")]);
         $service2 = factory(Servicio::class)->create(['hora_regreso'=>$horaRegreso2->format("Y-m-d H:i:s")]);
         $service3 = factory(Servicio::class)->create(['hora_regreso'=>$horaRegreso3->format("Y-m-d H:i:s")]);
 
-        //Modelo a probar
+
         $servicesYear = Servicio::Fecha(null,'2015');
         $servicesMonth = Servicio::Fecha('12', null);
         $servicesMonthYear = Servicio::Fecha('12', '2013');
-        //Assert
+
+
         $this->assertEquals($servicesYear->first()->id,$service1->id);
         $this->assertEquals($servicesYear->first()->hora_regreso, $service1->hora_regreso);
         $this->assertEquals($servicesYear->count(),1);
@@ -70,17 +67,14 @@ class ServiceTest extends TestCase
 
     public function testScopeFechaAlarma()
     {
-        //Setup
         $horaAlarma = new DateTime("2015-11-01 00:00:00", new DateTimeZone("America/New_York"));
         $service = factory(Servicio::class)->create(['hora_alarma'=>$horaAlarma->format("Y-m-d H:i:s")]);
-        $service2 = factory(Servicio::class)->create(['hora_alarma'=>$horaAlarma->format("Y-m-d H:i:s")]);
-        //Modelo a probar
+
         $servicesMonthYear = Servicio::FechaAlarma('12', '2013');
         $servicesMonthYear2 = Servicio::FechaAlarma('11', '2015');
-        //Assert
+
         $this->assertEquals($servicesMonthYear->count(),0);
         $this->assertEquals($servicesMonthYear2->count(),2);
-
       }
 
 }
