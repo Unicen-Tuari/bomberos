2ad059b8ca7ca3b2419e61648d69151fa8634f37
Alejandro Perez <alecio88@gmail.com>
2018-08-28 20:55:08 -0300

    chequeo de aparicion de boton listas de usuarios

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+
+    public function testShowUserMenu()
+    {
+      $user = factory(\App\User::class)->create();
+    
+
+      $response = $this->actingAs($user)
+                       ->get('/home');
+
+      $response->assertStatus(200)
+               ->assertDontSee('Lista de usuarios');
+    }
+
+    public function testShowUserMenuAsAdmin()
+    {
+      $user = factory(\App\User::class)->create(['admin'=>true]);
+
+      $response = $this->actingAs($user)
+                       ->get('/home');
+
+      $response->assertStatus(200)
+               ->assertSee('Lista de usuarios');
+    }
+
+}
