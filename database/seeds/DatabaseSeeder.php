4d69c1a15b3e07e2ffd566b96bab804559adad5e
Ignacio Jonas <ignaciojonas@gmail.com>
2018-09-03 11:37:28 -0300

    Create db command (#233)
    * Adding Default User to the Database
    
    * Create db:create command
    
    * Fix Travis by removing .env.testing file
    
    * Add Codecov Ignore
    
    * Update readme.md

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(BomberoTableSeeder::class);
         $this->call(TipoAsistenciaTableSeeder::class);
         $this->call(VehiculoTableSeeder::class);
