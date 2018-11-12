e3366fdf3e7ff5b3793de1241728bb74bb8404d6
Mariel Mangieri <marupipi@gmail.com>
2018-11-12 13:31:31 -0300

    Creación de tabla Planilla

---
+++ app/Planilla.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+//use App\Renglon;
+
+class Planilla extends Model
+{
+    protected $table = 'bombero';
+    protected $fillable = [
+      'id', 'jefe_guardia', 'nro_guardia', 'inicio_semana', 'fin_semana', 'mes',
+    ];
+
+
+
+    // public function renglones(){
+    //     return $this->hasMany(Renglon::class);
+    //   }
+}
---
+++ database/migrations/2018_11_12_153910_create_planillas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlanillasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planillas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('jefe_guardia',100);
+            $table->integer('nro_guardia');
+            $table->integer('inicio_semana');
+            $table->integer('fin_semana');
+            $table->string('mes',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('planillas');
+    }
+}
