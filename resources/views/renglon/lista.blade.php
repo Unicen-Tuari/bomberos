4a475f852d5d5864df5ad2aceaa1863b7a4b667c
Mariel Mangieri <marupipi@gmail.com>
2018-11-18 12:40:35 -0300

    agregue que se pueda agregar nuevos renglones desde el listar

--- resources/views/renglon/lista.blade.php
+++ resources/views/renglon/lista.blade.php
@@ -32,14 +32,17 @@
               </form>
           </td>
         </tr>
+        @php $planilla=$renglon->planilla_id @endphp
         @endforeach
-      </tbody>
+        </tbody>
+      
       <br>
     </table>
     <div class="form-group">
             <label class="col-md-4 control-label"  > Nuevo dato</label>
             <div class="col-md-6">            
-            <button type="submit" class="btn btn-success"> <a href="" class= "glyphicon glyphicon-plus"></a>
+            
+            <button type="submit" class="btn btn-success"> <a href="{{route('renglon.create',$planilla)}}" class= "glyphicon glyphicon-plus"></a>
             </button>
             </div>
          </div>  
